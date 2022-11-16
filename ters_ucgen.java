5ecac43dc79012f6612dc8541a5db658e6813c84
fur41 <115677824+fur41@users.noreply.github.com>
2022-11-17 02:59:29 +0300

    Add files via upload

---
+++ ters_ucgen.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ters_ucgen {
+  public static void main(String[] args) {
+
+    Scanner inp=new Scanner(System.in);
+    for (int i = 1; i <10; i++) {
+
+        for (int j = i; j < 10; j++) {System.out.print(" ");}
+          for (int k = 1; k <=i; k++) {System.out.print("*");
+          System.out.print(" ");
+          }  
+          System.out.println(" ");  } 
+for (int i = 10; 0 < i; i--) {
+ for (int j = i; j < 10; j++) {System.out.print(" ");
+
+ }   
+for (int k = 1; k <= i; k++) {
+    System.out.print("*");
+    System.out.print(" ");
+}
+System.out.println(" ");} for (int i = 1; i <10; i++) {
+
+    for (int j = i; j < 10; j++) {System.out.print(" ");}
+      for (int k = 1; k <=i; k++) {System.out.print("*");
+      System.out.print(" ");
+      }  
+      System.out.println(" ");  }
+      for (int i = 10; 0 < i; i--) {
+        for (int j = i; j < 10; j++) {System.out.print(" ");
+       
+        }   
+       for (int k = 1; k <= i; k++) {
+           System.out.print("*");
+           System.out.print(" ");
+       }
+       System.out.println(" ");}
+
+       for (int i = 1; i <10; i++) {
+
+        for (int j = i; j < 10; j++) {System.out.print(" ");}
+          for (int k = 1; k <=i; k++) {System.out.print("*");
+          System.out.print(" ");
+          }  
+          System.out.println(" ");  }
+          for (int i = 10; 0 < i; i--) {
+            for (int j = i; j < 10; j++) {System.out.print(" ");
+           
+            }   
+           for (int k = 1; k <= i; k++) {
+               System.out.print("*");
+               System.out.print(" ");
+           }
+           System.out.println(" ");}inp.nextShort();}}
